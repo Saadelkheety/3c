5276a063fafc5a0fa36e10b1c9ed075107f5a033
Saad Elkheety <saadelkheety@gmail.com>
2022-10-18 19:17:27 +0200

    static_collected for production collected static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -168,10 +168,10 @@ STATICFILES_FINDERS = [
 # See https://docs.djangoproject.com/en/3.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_URL = '/static/'
