9c094e4526d590d3b5821fb21727b69835bca989
Saad Elkheety <saadelkheety@gmail.com>
2022-10-23 07:09:10 +0200

    add form to home page

--- home/models.py
+++ home/models.py
@@ -1,18 +1,54 @@
 from django.db import models
-
-from wagtail.core.models import Page
-
-
+from modelcluster.fields import ParentalKey
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
 
 
-class HomePage(Page):
+class FormField(AbstractFormField):
+    page = ParentalKey('HomePage', on_delete=models.CASCADE, related_name='form_fields')
+
+class HomePage(AbstractEmailForm):
     template = 'index.html'
     body = RichTextField(blank=True)
-    content_panels = Page.content_panels + [
+    thank_you_text = RichTextField(blank=True)
+
+    thank_you_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+    def render_landing_page(self, request, form_submission=None, *args, **kwargs):
+        if self.thank_you_page:
+            url = self.thank_you_page.url
+            # if a form_submission instance is available, append the id to URL
+            # when previewing landing page, there will not be a form_submission instance
+            if form_submission:
+              url += '?id=%s' % form_submission.id
+            return redirect(url, permanent=False)
+        # if no thank_you_page is set, render default landing page
+        return super().render_landing_page(request, form_submission, *args, **kwargs)
+
+
+    content_panels = AbstractEmailForm.content_panels + [
         FieldPanel('body', classname="full"),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        PageChooserPanel('thank_you_page'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email Notification Config"),
     ]
+
+    def get_form_fields(self):
+        return self.form_fields.all()
